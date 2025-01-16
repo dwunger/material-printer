8d8bc61bf0f40944967194248e68aa3f7513004c
Denton Unger <106967706+dwunger@users.noreply.github.com>
2025-01-16 04:01:48 -0500

    Buffer gets mangled a bit between scripts

--- qc_labels.ps1
+++ qc_labels.ps1
@@ -5,6 +5,8 @@ using module '.\src\ScreenManager.psm1'
 using module '.\huginn\utils.psm1'
 using module '.\huginn\VerboseUpdateLogger.psm1'
 
+Clear-Host # Please don't tell me how to name my functions, Microsoft.
+
 $REMOTE_VERSION = Query-RemoteManifest -Parameter 'VERSION'
 $LOCAL_VERSION = Query-Parameter -File '.\MANIFEST' -Parameter 'VERSION'
 
@@ -14,7 +16,7 @@ $DEBUG = $false
 if ($LOCAL_VERSION -lt $REMOTE_VERSION) {
     
     Update-ClientVerbose
-
+    Clear-Host
     #$UpdateClient = Monitored_Start -Process 'powershell' -Args '-ExecutionPolicy Bypass -File .\Update.ps1'
     #
     # while (!$UpdateClient.HasExited) {
