8441e9bd4fdf66fe9cfa71ca193b0a407e4c14a0
Denton Unger <106967706+dwunger@users.noreply.github.com>
2025-02-26 21:13:52 -0500

    formatting only

--- src/app.ps1
+++ src/app.ps1
@@ -74,7 +74,7 @@ if ($DISABLE_PRINT){
     $global:VERSION += "$RED_FG - Printing is Disabled in Debug Mode."
 } 
 
-$STARTUP_LOGMSG = "- 's' - Snek`n- Overhaul!`n- Huginn SemVer bug fix"
+$STARTUP_LOGMSG = "- 's' - Snek Overhaul!`n- Huginn SemVer bug fix"
 $STARTUP_LOGMSG = $STARTUP_LOGMSG -replace "`n", "`n$YELLOW_FG"
 
 # Import-Module command with detailed parameter explanation
