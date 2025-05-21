6bfafc259d59612ba7cf85b4116c6396fcc04640
Denton Unger <106967706+dwunger@users.noreply.github.com>
2025-05-20 23:33:43 -0400

    Added Novus SG well cleaning solution label to core lab section

--- src/app.ps1
+++ src/app.ps1
@@ -88,7 +88,7 @@ if ($DISABLE_PRINT){
     $global:VERSION += "$RED_FG - Printing is Disabled in Debug Mode."
 } 
 
-$STARTUP_LOGMSG = "- Added Wash buffer SDS label,`n  send-out stickers, and tired Pepe`n- Added Advanced Help option [?]`n- Simple Atari Frog Bog port to`n  WinForms [g]`n- Added BB Printers`n- Added BB phenotyping instructions`n- Added Downtime Barcode printer"
+$STARTUP_LOGMSG = "- Added Novus SG Well,`n  clean solution to`n  core lab section"
 $STARTUP_LOGMSG = $STARTUP_LOGMSG -replace "`n", "`n$YELLOW_FG"
 
 # Import-Module command with detailed parameter explanation
