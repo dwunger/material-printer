6ab006f28846e2841f546666d29223ff0c5873e3
Denton Unger <106967706+dwunger@users.noreply.github.com>
2025-08-28 02:07:29 -0400

    Downtime barcode printer update

--- src/app.ps1
+++ src/app.ps1
@@ -88,7 +88,7 @@ if ($DISABLE_PRINT){
     $global:VERSION += "$RED_FG - Printing is Disabled in Debug Mode."
 } 
 
-$STARTUP_LOGMSG = "- Now copies ISE calibrator barcodes`n  when scanned`n- Added conservative rounding for `n  near-midnight prints`n- Added special clean labels`n"
+$STARTUP_LOGMSG = "- Google's endpoints for online`n  resources are back up`n- Added copies and scan features`n  to downtime barcode printer"
 
 # prepend the orange alert, then color the body bright yellow
 $STARTUP_LOGMSG = $STARTUP_LOGMSG -replace "`n", "`n$CYAN_FG"
